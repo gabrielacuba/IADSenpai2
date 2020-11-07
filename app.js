14ec0f1d5e6778997fc1728518caea580c2a1606
gabrielacuba <gabicub@gmail.com>
2020-11-07 15:22:49 -0300

    terminando estructura de carpeta

--- app.js
+++ app.js
@@ -2,11 +2,25 @@ const express = require("express");
 const router = require("express");
 const app = express();
 
+const bodyParser= require('body-parser');
+const cors= require('cors');
+const morgan = require('morgan');
 
-app.get("/", async (request, response)=> {
-    /*const data = await procesarData(); */
+const corsOptions={
+    methods: 'GET,PUT,POST,DELETE,OPTIONS',
+    origin:'*',
+    allowedHeaders:['Content-Type', 'Authorization', 'access_token', 'ACCESS_TOCKEN'],
+    exposedHeaders:['Content-Type', 'Authorization', 'access_token', 'ACCESS_TOCKEN'],
+};
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+app.use(express.static(__dirname + '/public'));
+
+/* app.get("/", async (request, response)=> {
+    //const data = await procesarData(); 
     response.send("Mba'eichapa");
-})
+}) */
 
 const routes =require("./routes/routes");
 app.use('/api/v1', routes);
